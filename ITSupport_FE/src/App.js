35884792c98819ae6daa767bed86ebfd1f3f4ad2
calebjchang <caleb33@hawaii.edu>
2023-10-30 16:19:27 -1000

    create-react-app init
    - npx create-react-app to start up
    - ripped site design from figma
    - only app & index, no components set up yet,

---
+++ ITSupport_FE/src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div className="dark-mode-vers">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="title-box" />
+          <p className="text-wrapper">
+            ITSupport is an intuitive AI bot that will provide you the answers to your questions. Ask a question below.
+          </p>
+          <div className="text-wrapper-2">Welcome to ITSupport!</div>
+          <img className="vector" alt="Vector" src="vector-1.svg" />
+        </div>
+        <div className="overlap">
+          <div className="message-input">Send a message</div>
+        </div>
+        <div className="overlap-2">
+          <div className="white-bar-top" />
+          <img className="UH-logo" alt="Uh logo" src="UH-logo.png" />
+          <img className="navbar-subsitute" alt="Navbar subsitute" src="navbar-subsitute.png" />
+          <img className="ITS-logo" alt="Its logo" src="ITS-logo.png" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
